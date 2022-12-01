f26f3ae9eaa031da06c74f8928b84468945e238a
Jungtaek Kim <jungtaek.kim@pitt.edu>
2022-12-01 12:55:58 -0500

    Add some arguments

--- src/run_batch_bo.py
+++ src/run_batch_bo.py
@@ -8,23 +8,28 @@ import bayeso_benchmarks.utils as bb_utils
 from bbo import BBOLocalPenalization
 
 
-def run_batch_bo_single_iteration(X, Y, bounds, size_batch):
-    model_bo = BBOLocalPenalization(bounds, size_batch)
+def run_batch_bo_single_iteration(str_method, X, Y, bounds, size_batch):
+    if str_method == 'local_penalization':
+        model_bo = BBOLocalPenalization(bounds, size_batch)
+    else:
+        raise ValueError
 
     X_batch, dict_info = model_bo.optimize(X, Y)
     time_overall = dict_info['time_overall']
 
     return X_batch, time_overall
 
-def run_batch_bo(objective, bounds, size_batch):
-    X = np.random.RandomState().uniform(size=(size_batch, bounds.shape[0])) * (bounds[:, 1] - bounds[:, 0]) + bounds[:, 0]
+def run_batch_bo(str_method, objective, bounds, size_batch, seed=None):
+    X = np.random.RandomState(seed).uniform(size=(size_batch, bounds.shape[0])) * (bounds[:, 1] - bounds[:, 0]) + bounds[:, 0]
     Y = objective(X)
     times = np.array([0.0])
 
     print(X)
 
     for ind_iter in range(0, num_iter):
-        X_batch, time_overall = run_batch_bo_single_iteration(X, Y, bounds, size_batch)
+        X_batch, time_overall = run_batch_bo_single_iteration(
+            str_method, X, Y, bounds, size_batch
+        )
         print(f'Iteration {ind_iter + 1}')
         print('X_batch')
         print(X_batch)
@@ -39,17 +44,23 @@ def run_batch_bo(objective, bounds, size_batch):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--method', type=str, required=True)
     parser.add_argument('--target', type=str, required=True)
     parser.add_argument('--size_batch', type=int, required=True)
     parser.add_argument('--num_iter', type=int, required=True)
 
     args = parser.parse_args()
 
+    str_method = args.method
     str_target = args.target
     size_batch = args.size_batch
     num_iter = args.num_iter
 
-    num_separators = 20
+    assert str_method in [
+        'local_penalization',
+    ]
+
+    num_separators = 25
 
     list_str_target = str_target.split('_')
     if len(list_str_target) == 1:
@@ -62,14 +73,15 @@ if __name__ == '__main__':
     print('=' * num_separators + 'START' + '=' * num_separators)
 
     bounds = obj_target.get_bounds()
-    print('bounds')
+    print('Bounds')
     print(bounds)
+    print('')
 
     def fun_target(X):
         Y = obj_target.output(X)
         return Y
 
-    X, Y, times = run_batch_bo(fun_target, bounds, size_batch)
+    X, Y, times = run_batch_bo(str_method, fun_target, bounds, size_batch)
 
     print(f'X.shape {X.shape} Y.shape {Y.shape} times.shape {times.shape}')
     print('=' * num_separators + 'END' + '=' * num_separators)
--- src/script_run_batch_bo.sh
+++ src/script_run_batch_bo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+METHOD='local_penalization'
 TARGET='branin'
 SIZE_BATCH=5
 NUM_ITER=20
 
-python run_batch_bo.py --target $TARGET --size_batch $SIZE_BATCH --num_iter $NUM_ITER
+python run_batch_bo.py --method $METHOD --target $TARGET --size_batch $SIZE_BATCH --num_iter $NUM_ITER
