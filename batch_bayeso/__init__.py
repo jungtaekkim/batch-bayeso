ee9451ee5ee89f20d7486949f64499dab1f7e666
Jungtaek Kim <jungtaek.kim@pitt.edu>
2022-12-13 16:04:51 -0500

    Change a file name

--- batch_bayeso/__init__.py
+++ batch_bayeso/__init__.py
@@ -6,6 +6,6 @@
 from batch_bayeso.batch_bo_random import BBORandom
 from batch_bayeso.batch_bo_constant import BBOConstant
 from batch_bayeso.batch_bo_prediction import BBOPrediction
-from batch_bayeso.batch_bo_lp import BBOLocalPenalization
+from batch_bayeso.batch_bo_local_penalization import BBOLocalPenalization
 
 from batch_bayeso.wrappers_batch_bo import BatchBayesianOptimization
